9797bc35fb3d55d2052774164602458d5714ea68
BoemoM5CS <mogaleboemo@gmail.com>
2026-04-15 14:47:02 +0200

    Update next.config.js
    update on keys and url

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_SUPABASE_URL: "https://rjiysvvlbgrwbkzzzfki.supabase.co",
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJqaXlzdnZsYmdyd2Jrenp6ZmtpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYwNTM1NDEsImV4cCI6MjA5MTYyOTU0MX0.4ZmvXhk_zPFtA1X_TBfXV8LRaEGb-_AobuqyQoN6ikM",
+    NEXT_PUBLIC_SUPABASE_URL: "https://qjojwsboebdzpyhgsmlv.supabase.co",
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFqb2p3c2JvZWJkenB5aGdzbWx2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ4MDYwODMsImV4cCI6MjA5MDM4MjA4M30.riHUOkn0gBE4xW49N2WJAX_Kah6TtzhLUXDJz9g2C-w",
   },
   images: { unoptimized: true },
   optimizeFonts: false,
